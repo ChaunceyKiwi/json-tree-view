381c940946ddde9d4ba65302ba9a8f032b2fda79
ChaunceyKiwi <chaunceykiwi@gmail.com>
2018-04-03 13:53:44 -0700

    Publish the first version of tree view

---
+++ out/extension.js
@@ -0,0 +1,22 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const jsonOutline_1 = require("./jsonOutline");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    const jsonOutlineProvider = new jsonOutline_1.JsonOutlineProvider(context);
+    vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
+    vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
+    vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
+    vscode.commands.registerCommand('jsonOutline.renameNode', offset => jsonOutlineProvider.rename(offset));
+    vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
---
+++ src/extension.ts
@@ -0,0 +1,21 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { JsonOutlineProvider } from './jsonOutline';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    const jsonOutlineProvider = new JsonOutlineProvider(context);
+
+    vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
+	vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
+	vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
+	vscode.commands.registerCommand('jsonOutline.renameNode', offset => jsonOutlineProvider.rename(offset));
+	vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
